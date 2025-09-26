ae65119c39c6696cad00a76a3b8a29e5826227d5
wanthinkali <160533813+wanthinkali@users.noreply.github.com>
2025-09-26 10:03:07 +0300

    Add files via upload

--- Lab1/Purple.cs
+++ Lab1/Purple.cs
@@ -37,7 +37,25 @@
             bool answer = false;
 
             // code here
+            bool Task2(int a, int b)
+            {
 
+                if (b != 0 && a % b == 0)
+                {
+                    return true;
+                }
+                else if (a != 0 && b % a == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            answer = Task2(a, b);
             // end
 
             return answer;
@@ -48,6 +66,24 @@
 
             // code here
 
+            bool Task3(int a, int b)
+            {
+
+                if (Math.Pow(a, 2) == b || Math.Pow(b, 2) == a)
+                {
+                    return true;
+                }
+                else if (Math.Pow(a, 3) == b || Math.Pow(b, 3) == a)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            answer = Task3(a, b);
             // end
 
             return answer;
@@ -58,6 +94,15 @@
 
             // code here
 
+            double Task4(double d, double f, double g)
+            {
+                double x = (Math.Pow(f, 2) - 4 * d * g);
+
+                return x;
+
+            }
+
+            answer = Task4(d, f, g);
             // end
 
             return answer;
@@ -67,7 +112,23 @@
             double answer = 0;
 
             // code here
+            double Task5(double x)
+            {
+                if (x <= -1)
+                {
+                    return 1;
+                }
+                else if (x > -1 && x <= 1)
+                {
+                    return -x;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
 
+            answer = Task5(x);
             // end
 
             return answer;
@@ -77,7 +138,23 @@
             bool answer = false;
 
             // code here
+            bool Task6(double sqar, double circar)
+            {
+                double diagonal = Math.Sqrt(sqar + sqar);
+                double radius = diagonal / 2;
 
+                if (Math.PI * (Math.Pow(radius, 2)) <= circar)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            answer = Task6(squareS, circleS);
             // end
 
             return answer;
@@ -88,7 +165,50 @@
             int answer = 0;
 
             // code here
+            int Task7(bool s, bool t, bool f)
+            {
 
+                if (s)
+                {
+                    if (t)
+                    {
+                        answer = 6;
+                    }
+                    else
+                    {
+                        if (f)
+                        {
+                            answer = 10;
+                        }
+                        else
+                        {
+                            answer = 2;
+                        }
+                    }
+                }
+                else
+                {
+                    if (t)
+                    {
+                        answer = 3;
+                    }
+                    else
+                    {
+                        if (f)
+                        {
+                            answer = 5;
+                        }
+                        else
+                        {
+                            answer = 1;
+                        }
+                    }
+                }
+
+                return answer;
+            }
+
+            Task7(s, t, f);
             // end
 
             return answer;
@@ -99,7 +219,73 @@
             const int bank = 10000;
 
             // code here
-            
+            bool Task8(int y, int studs, int pay)
+            {
+
+                if (y % 4 == 0)
+                {
+                    if (y % 100 == 0)
+                    {
+                        if (y % 400 == 0)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            //
+                            int auroras = 0;
+
+                            if (studs % 7 == 0)
+                            {
+                                auroras = studs / 7;
+                            }
+                            else
+                            {
+                                auroras = (studs / 7) + 1;
+                            }
+
+                            if (((auroras * pay) + (studs * 5)) <= 10000)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    //
+                    int auroras = 0;
+
+                    if (studs % 7 == 0)
+                    {
+                        auroras = studs / 7;
+                    }
+                    else
+                    {
+                        auroras = (studs / 7) + 1;
+                    }
+
+                    if (((auroras * pay) + (studs * 5)) <= 10000)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+            }
+
+            answer = Task8(year, pupils, salary);
             // end
 
             return answer;
